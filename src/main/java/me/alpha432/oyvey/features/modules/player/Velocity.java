f83a0b2ee9a48392013bd02cefb211f9b6934937
JinoStk123 <163722693+JinoStk123@users.noreply.github.com>
2025-07-10 14:20:05 +0700

    Update Velocity.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
@@ -1,8 +1,8 @@
-package me.alpha432.oyvey.features.modules.player;
+package me.alpha432.gaps.features.modules.player; // Đã đổi package thành gaps
 
 import com.google.common.eventbus.Subscribe;
-import me.alpha432.oyvey.event.impl.PacketEvent;
-import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.gaps.event.impl.PacketEvent; // Đã đổi import từ oyvey.event sang gaps.event
+import me.alpha432.gaps.features.modules.Module; // Đã đổi import từ oyvey.features sang gaps.features
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 
@@ -11,7 +11,8 @@ public class Velocity extends Module {
         super("Velocity", "Removes velocity from explosions and entities", Category.PLAYER, true, false, false);
     }
 
-    @Subscribe private void onPacketReceive(PacketEvent.Receive event) {
+    @Subscribe
+    private void onPacketReceive(PacketEvent.Receive event) {
         if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket || event.getPacket() instanceof ExplosionS2CPacket) event.cancel();
     }
 }
