725cfcf71cc2a9cecf71cae1c3fbfd548a5a3017
JinoStk123 <163722693+JinoStk123@users.noreply.github.com>
2025-07-10 14:16:47 +0700

    Update Criticals.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Criticals.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Criticals.java
@@ -9,30 +9,43 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.Vec3d;
 
 public class Criticals extends Module {
     private final Timer timer = new Timer();
+    private int packetMode = 0;
+
     public Criticals() {
-        super("Criticals", "Makes you do critical hits", Category.COMBAT, true, false, false);
+        super("Criticals", "Makes you do critical hits subtly for ghost play", Category.COMBAT, true, false, false);
     }
-    @Subscribe private void onPacketSend(PacketEvent.Send event) {
+
+    @Subscribe
+    private void onPacketSend(PacketEvent.Send event) {
         if (event.getPacket() instanceof PlayerInteractEntityC2SPacket packet && packet.type.getType() == PlayerInteractEntityC2SPacket.InteractType.ATTACK) {
             Entity entity = mc.world.getEntityById(packet.entityId);
+
             if (entity == null
                     || entity instanceof EndCrystalEntity
                     || !mc.player.isOnGround()
-                    || !(entity instanceof LivingEntity)
-                    || !timer.passedMs(0)) return;
+                    || mc.player.isFallFlying()
+                    || mc.player.isInSwimmingPose()
+                    || mc.player.hasVehicle()
+                    || !timer.passedMs(mc.player.getAttackCooldownProgress(0.5F) >= 1.0f ? 250 : 500)) {
+                return;
+            }
 
-            boolean bl = mc.player.horizontalCollision;
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), mc.player.getY() + (double) 0.1f, mc.player.getZ(), false, bl));
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), mc.player.getY(), mc.player.getZ(), false, bl));
+            double yOffset = 0.000000000000001;
+
+            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), mc.player.getY() + yOffset, mc.player.getZ(), false));
+            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), mc.player.getY(), mc.player.getZ(), false));
+            
             mc.player.addCritParticles(entity);
             timer.reset();
         }
     }
 
-    @Override public String getDisplayInfo() {
-        return "Packet";
+    @Override
+    public String getDisplayInfo() {
+        return "Ghost";
     }
 }
